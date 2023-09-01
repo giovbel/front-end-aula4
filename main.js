64e3eb50437d875976f64c89fde8efec40caa51a
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2023-09-01 13:44:25 -0300

    terminado

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const botaoMostrarTriplo = document.getElementById('botao-mostrar-numeros-triplo
 const botaoMostrarProximoImpar = document.getElementById('botao-mostrar-proximo-impar')
 const botaoMostrarMultiploE = document.getElementById('botao-mostrar-multiplos')
 const botaoMostrarMultiplo = document.getElementById('botao-mostrar-multiplos-ou')
+const botaoMostrarProximoPrimo = document.getElementById('botao-mostrar-proximo-primo')
 
 function mostrarNumeros() {
 
@@ -120,9 +121,48 @@ function mostrarMultiplo() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+function ePrimo(numero) {
+    if (numero <= 1) return false
+
+
+    if (numero <= 3) return true
+
+
+    if (numero % 2 === 0 || numero % 3 === 0) return false
+
+
+    for (let contador = 2; contador < numero; contador++) {
+        if (numero % contador === 0) return false
+    }
+    return true
+}
+
+
+function mostrarProximoPrimo() {
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-mostrar-proximo-primo')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for (let contador = 0; contador < ultimoIndice; contador++) {
+
+        for (var proximoPrimo = Number (numeros[contador]) + 1; ePrimo(proximoPrimo) == false; proximoPrimo++){
+        }
+
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = proximoPrimo
+            container.appendChild(novoSpan)
+        }
+    }
+
+
 botaoMostrarNumeros.addEventListener('click', mostrarNumeros)
 botaoMostrarNumerosPares.addEventListener('click', mostrarNumerosPares)
 botaoMostrarTriplo.addEventListener('click', mostrarTriplo)
 botaoMostrarProximoImpar.addEventListener('click', mostrarProximoImpar)
 botaoMostrarMultiploE.addEventListener('click', mostrarMultiploE)
 botaoMostrarMultiplo.addEventListener('click', mostrarMultiplo)
+botaoMostrarProximoPrimo.addEventListener('click', mostrarProximoPrimo)
